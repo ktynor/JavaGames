703cfde359a8960d0e68386336ec1c9cd5ee6971
ktynor <63809493+ktynor@users.noreply.github.com>
2022-09-25 11:40:17 +0100

    Update Main.java

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -4,10 +4,6 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> userNumbers=UserNumbers.getUserNumbers();
-        RandomNumbers randomNumbers = new RandomNumbers();
-        System.out.println(randomNumbers.getRandomNumbers());
-        System.out.println(randomNumbers.getRandomNumbers());
-
+       
     }
 }
